2342fa1c14f63ece74e423d55aacf33ef1f01a69
borzois <36570861+borzois@users.noreply.github.com>
2022-05-30 13:53:28 +0300

    booru cog after haircut

--- cogs/booru.py
+++ cogs/booru.py
@@ -4,6 +4,15 @@ import io
 import aiohttp
 
 
+async def dl_img(image):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(image) as resp:
+            if resp.status != 200:
+                raise aiohttp.ServerConnectionError  # ?
+            else:
+                return io.BytesIO(await resp.read())
+
+
 class Booru(commands.Cog):
     def __init__(self, client, booru_tool):
         self.client = client
@@ -11,35 +20,30 @@ class Booru(commands.Cog):
 
     @commands.command()
     async def gayporn(self, ctx):
-        image = self.booru.get_random("yaoi")
-        async with aiohttp.ClientSession() as session:
-            async with session.get(image) as resp:
-                if resp.status != 200:
-                    return await ctx.send('could not download file...')
-                data = io.BytesIO(await resp.read())
-                await ctx.send(file=disnake.File(data, 'gay.png'))
+        image_query = self.booru.get_random("yaoi")
+        try:
+            data = await dl_img(image_query)
+            await ctx.send(file=disnake.File(data, 'obama.png'))
+        except aiohttp.ServerConnectionError:
+            ctx.send("Could not download")
 
     @commands.command()
     async def yuri(self, ctx):
-        image = self.booru.get_random("yuri")
-        async with aiohttp.ClientSession() as session:
-            async with session.get(image) as resp:
-                if resp.status != 200:
-                    return await ctx.send('could not download file...')
-                data = io.BytesIO(await resp.read())
-                await ctx.send(file=disnake.File(data, 'gay.png'))
+        image_query = self.booru.get_random("yuri")
+        try:
+            data = await dl_img(image_query)
+            await ctx.send(file=disnake.File(data, 'obama.png'))
+        except aiohttp.ServerConnectionError:
+            ctx.send("Could not download")
 
     @commands.command()
     async def booru(self, ctx, *args):
         tags = ' '.join(args)
         print(ctx.author.name + " has requested " + tags)
 
-        image = self.booru.get_random(tags)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(image) as resp:
-                if resp.status != 200:
-                    return await ctx.send('could not download file...')
-                data = io.BytesIO(await resp.read())
-                await ctx.send(file=disnake.File(data, 'gay.png'))
-
-    
+        image_query = self.booru.get_random(tags)
+        try:
+            data = await dl_img(image_query)
+            await ctx.send(file=disnake.File(data, 'obama.png'))
+        except aiohttp.ServerConnectionError:
+            ctx.send("Could not download")
